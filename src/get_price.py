688ffbe81ae759e977728f1aec4ed3be9a035c74
mateus-de-paula <168784423+mateus-de-paula@users.noreply.github.com>
2025-03-26 19:08:05 -0300

    Driffle and Eneba
    -added a filter for Driffle and Eneba codes
    -.exe file updated
    -added requirements.txt

--- src/get_price.py
+++ src/get_price.py
@@ -13,6 +13,7 @@ def get_price(game_url):
         # Captura o país
         first_country = soup.select('body > div.wrapper.main-wrapper > div.well > table > tbody > tr:nth-child(1) > td:nth-child(2)')[0].get_text().strip()
         second_country = soup.select('body > div.wrapper.main-wrapper > div.well > table > tbody > tr:nth-child(2) > td:nth-child(2)')[0].get_text().strip()
+        third_country = soup.select('body > div.wrapper.main-wrapper > div.well > table > tbody > tr:nth-child(3) > td:nth-child(2)')[0].get_text().strip()
         country = first_country
 
         # Captura o preço
@@ -20,15 +21,18 @@ def get_price(game_url):
         discount = element.find('div', class_="discounted")
 
         # Tira Argentina da jogada já que não podemos comprar la
-        if "Argentina" in first_country and discount:
+        if "Argentina" in first_country or "Driffle" in first_country or "Eneba" in first_country and discount:
             price_list = soup.find_all('del', limit=2)
             original_price = price_list[1]
             country = second_country
-        elif "Argentina" in first_country and not discount:
+        elif "Argentina" in first_country or "Driffle" in first_country or "Eneba" in first_country and not discount:
             original_price = soup.find('del')
             country = second_country
         else:
             original_price = soup.find('del')
+
+        if "Driffle" in country or "Eneba" in country:
+            country = third_country
 
         if original_price:
             discounted_price = original_price.find_next_sibling(string=True)
